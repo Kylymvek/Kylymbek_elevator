21a01ced56612f322c5348c06c9f139cda63f317
Kylymvek <kylymbek.muratov.00@gmail.com>
2022-09-25 01:54:57 +0600

    elevator

---
+++ main.js
@@ -0,0 +1,86 @@
+let container = document.querySelector(".container")[0];
+let home = document.querySelector("#home")[0];
+let lift = document.querySelector(".container__lift");
+let containerBtns = document.querySelector(".container__btns");
+
+let floor1 = document.getElementById("1-floor");
+let floor2 = document.getElementById("2-floor");
+let floor3 = document.getElementById("3-floor");
+let floor4 = document.getElementById("4-floor");
+let floor5 = document.getElementById("5-floor");
+let floor6 = document.getElementById("6-floor");
+let floor7 = document.getElementById("7-floor");
+let floor8 = document.getElementById("8-floor");
+let floor9 = document.getElementById("9-floor");
+let floor10 = document.getElementById("10-floor");
+let floor11 = document.getElementById("11-floor");
+let floor12 = document.getElementById("12-floor");
+let floor13 = document.getElementById("13-floor");
+let floor14 = document.getElementById("14-floor");
+let floor15 = document.getElementById("15-floor");
+let floor16 = document.getElementById("16-floor");
+
+floor1.addEventListener("click", () => {
+  lift.style.bottom = "24px";
+  lift.style.transition = "2.5s";
+});
+floor2.addEventListener("click", () => {
+  lift.style.bottom = "92px";
+  lift.style.transition = "2.5s";
+});
+floor3.addEventListener("click", () => {
+  lift.style.bottom = "130px";
+  lift.style.transition = "2.5s";
+});
+floor4.addEventListener("click", () => {
+  lift.style.bottom = "168px";
+  lift.style.transition = "2.5s";
+});
+floor5.addEventListener("click", () => {
+  lift.style.bottom = "206px";
+  lift.style.transition = "2.5s";
+});
+floor6.addEventListener("click", () => {
+  lift.style.bottom = "240px";
+  lift.style.transition = "2.5s";
+});
+floor7.addEventListener("click", () => {
+  lift.style.bottom = "278px";
+  lift.style.transition = "2.5s";
+});
+floor8.addEventListener("click", () => {
+  lift.style.bottom = "316px";
+  lift.style.transition = "2.5s";
+});
+floor9.addEventListener("click", () => {
+  lift.style.bottom = "352px";
+  lift.style.transition = "2.5s";
+});
+floor10.addEventListener("click", () => {
+  lift.style.bottom = "388px";
+  lift.style.transition = "2.5s";
+});
+floor11.addEventListener("click", () => {
+  lift.style.bottom = "426px";
+  lift.style.transition = "2.5s";
+});
+floor12.addEventListener("click", () => {
+  lift.style.bottom = "462px";
+  lift.style.transition = "2.5s";
+});
+floor13.addEventListener("click", () => {
+  lift.style.bottom = "500px";
+  lift.style.transition = "2.5s";
+});
+floor14.addEventListener("click", () => {
+  lift.style.bottom = "536px";
+  lift.style.transition = "2.5s";
+});
+floor15.addEventListener("click", () => {
+  lift.style.bottom = "572px";
+  lift.style.transition = "2.5s";
+});
+floor16.addEventListener("click", () => {
+  lift.style.bottom = "610px";
+  lift.style.transition = "2.5s";
+});
